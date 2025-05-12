a5a9cad217c9873c6aec91bc2a53f1bc5cab9056
jaehyeok34 <hyeok9942@gmail.com>
2025-05-12 13:37:10 +0900

    "/get-data" 엔드포인트에 GET 요청시 csv 파일 내용을 반환"

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+import pandas as pd
+from flask import request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "data-server"
+
+@app.route('/get-data', methods=['GET'])
+def get_data():
+    try:
+        df = pd.read_csv('datas.csv')
+        data = df.to_dict(orient='list')
+        return jsonify(data)
+    
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(port="7831", debug=True)
