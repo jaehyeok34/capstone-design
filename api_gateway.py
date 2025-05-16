9da829a2c10b3074c1a21f2e7c227e805329d208
jaehyeok34 <hyeok9942@gmail.com>
2025-05-16 20:40:09 +0900

    matching key

---
+++ api_gateway.py
@@ -0,0 +1,47 @@
+import time
+from typing import Dict, List
+import requests
+from dataclasses import dataclass
+
+@dataclass
+class EventDTO:
+    event: str
+    data: Dict[str, List[str]]
+
+
+def request_post(target_url: str, event: str, data: Dict[str, List[str]]) -> bool:
+    dto = EventDTO(event=event, data=data)
+    response = requests.post(url=target_url, json=dto.__dict__)
+
+    # API Gateway가 잘 받았는지 확인해야 하고, 그렇지 않을 경우 재요청 해야하는데,
+    # 시간이 없으니까 일단은 one line path만 구성
+    
+    return response.status_code == 200
+
+
+def subscribe(register_url: str, topic: str, callback_url: str, require_pii_data: bool = False, count: int = 1, interval: int = 0) -> bool:
+    default_interval = 10
+    interval = max(0, interval)
+
+    # 매개변수 체크
+    if (not topic) or (not callback_url):
+        raise ValueError('"topic"과 "callback_url"은 필수입니다.')
+    
+    # n번의 재시도 과정에서 interval이 0인 경우 default 10초로 설정
+    if (count > 1) and (interval == 0):
+        interval = default_interval
+
+    for i in range(count):
+        try:
+            response = requests.post(url=register_url, json={'topic': topic, 'url': callback_url, 'requirePiiData': require_pii_data})
+            if response.status_code == 200:
+                print(f"[debug]: gateway에 {topic} 등록 성공")
+                return True
+            
+        except Exception:
+            pass
+
+        print(f"[debug]: gateway에 {topic} {i}번 째 등록 실패")
+        time.sleep(interval)
+        
+    return False
--- app.py
+++ app.py
@@ -2,9 +2,8 @@ from flask import Flask
 import pandas as pd
 from flask import request, jsonify
 import hashlib
-import requests
-from typing import Dict, List, Any
-from register import Register
+from typing import Dict, Any
+from api_gateway import subscribe, request_post
 
 app = Flask(__name__)
 
@@ -12,20 +11,30 @@ app = Flask(__name__)
 def home():
     return "matching-key-server"
 
-
+"""
+input: {
+    "columns": ["v1", "v2", ...],
+    "piiData": {
+        "key1": ["v1", "v2", ...],
+        "key2": ["v1", "v2", ...],
+        ...
+    }
+}
+"""
 @app.route('/gen-matching-key', methods=['POST'])
 def generate_matching_key():
-    data: Dict[str, Dict[str, List[Any]]] = request.get_json()
+    data: Dict[str, Any] = request.get_json()
     if (
         not data or
-        'pii-data' not in data or
-        not isinstance(data['pii-data'], dict)
+        'columns' not in data or 
+        'piiData' not in data or 
+        not isinstance(data['piiData'], dict)
     ):
-        return jsonify({'error': '올바르지 않은 요청입니다. pii-data를 포함해야 합니다.'}), 400
+        return jsonify({'error': '올바르지 않은 요청입니다. columns와 pii-data를 포함해야 합니다.'}), 400
     
     # JSON 데이터에서 DataFrame 생성
     try:
-        df = pd.DataFrame(data['pii-data'])
+        df = pd.DataFrame(data['piiData'])
 
     except ValueError as e:
         return jsonify({'error': '올바르지 않은 JSON 형식입니다.'}), 400
@@ -35,22 +44,26 @@ def generate_matching_key():
         return hashlib.sha256(concatenated.encode('utf-8')).hexdigest()
 
     df['matching_key'] = df.apply(hash_row, axis=1)
+    print('[debug] df with matching_key:', df)
 
-    return df.to_json(orient='records'), 200
+    return "", 200
     
 
 if __name__ == '__main__':
-    register_url = 'http://localhost:1780/'
-    callback_url = 'http://localhost:1783/gen-matching-key'
+    port = 1783
+    register_url = 'http://localhost:1780/register'
+    callback_url = f'http://localhost:{port}/gen-matching-key'
 
-    ok = Register.subscribe(
+    ok = subscribe(
         register_url=register_url,
         topic='pii detection success',
         callback_url=callback_url,
+        require_pii_data=True,
         count=3,
         interval=5
     )
     if not ok:
         exit(1)
 
-    app.run(port=1783, debug=True)
+    # app.run(port=port, debug=True)
+    app.run(port=port)
