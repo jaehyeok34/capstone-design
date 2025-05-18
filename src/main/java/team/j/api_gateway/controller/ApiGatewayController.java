919bfdf5db28ff568f04e228c207b0ed7ef603b6
jaehyeok34 <hyeok9942@gmail.com>
2025-05-18 19:03:43 +0900

    api gateway

--- src/main/java/team/j/api_gateway/controller/ApiGatewayController.java
+++ src/main/java/team/j/api_gateway/controller/ApiGatewayController.java
@@ -1,17 +1,48 @@
 package team.j.api_gateway.controller;
 
+import java.io.IOException;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import team.j.api_gateway.service.ApiGatewayService;
-
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
 public class ApiGatewayController {
-    
+
+    private final ApiGatewayService service;
+
+    public ApiGatewayController(ApiGatewayService service) {
+        this.service = service;
+    }
+
     @GetMapping("/")
     public String home() {
         ApiGatewayService service = new ApiGatewayService();
         return service.home();
     }
+
+    @PostMapping("/register-database")
+    public ResponseEntity<Void> registerDatabase(@RequestBody String entity) {
+        // Todo: 나중에 개발할 거임
+
+        return ResponseEntity.ok().build();
+    }
+    
+    @PostMapping("/upload-csv")
+    public ResponseEntity<Void> uploadCSV(@RequestParam("file") MultipartFile csv) throws IOException {
+        if (csv.isEmpty()) {
+            return ResponseEntity.badRequest().build();
+        }
+        service.saveCSV(csv); 
+
+        return ResponseEntity.ok().build();
+    }
+    
+    
 }
--- src/main/java/team/j/api_gateway/service/ApiGatewayService.java
+++ src/main/java/team/j/api_gateway/service/ApiGatewayService.java
@@ -1,11 +1,29 @@
 package team.j.api_gateway.service;
 
+import java.io.File;
+import java.io.IOException;
+
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class ApiGatewayService {
 
     public String home() {
         return "API Gateway is running";
+    }
+
+    public void saveCSV(MultipartFile csv) throws IOException {
+        // String filePath = "./csv/" + csv.getOriginalFilename();
+        File dir = new File("./csv/");
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+
+        // dir에 csv파일 저장
+        String filePath = dir.getAbsolutePath() + File.separator + csv.getOriginalFilename();
+        File file = new File(filePath);
+
+        csv.transferTo(file);
     }
 }
