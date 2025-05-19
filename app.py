55e7d0dfeeb953f06b57e81593800b307db8ba5c
jaehyeok34 <hyeok9942@gmail.com>
2025-05-19 18:11:57 +0900

    pseudonymization server

--- app.py
+++ app.py
@@ -1,20 +1,30 @@
 import math
 import pandas as pd
 
-df = pd.read_csv('datas.csv')
-
-print(df.dtypes) 
-
-# "컬럼명": "타입"의 딕셔너리 구성
-for column, dtype in df.dtypes.items():
-    # object 타입은 마스킹 적용
-    if dtype == 'object':
-        def partial_masking(x):
+def pseudonymize_object(df: pd.DataFrame, column: str, rate: int = 0.7):
+    def partial_masking(x):
             if isinstance(x, str):
-                mask_len = math.ceil(int(len(x) * 0.7))
+                mask_len = math.ceil(int(len(x) * rate))
                 return x[:len(x)-mask_len] + '*' * mask_len
             
             return x
-        df[column] = df[column].apply(partial_masking)
+    df[column] = df[column].apply(partial_masking)
+
+
+def pseudonymize_numeric(df: pd.DataFrame, column: str):
+    n = len(df[column])
+    q = max(1, n // 2)
+    bins = pd.qcut(df[column], q=q, duplicates='drop')
+    df[column] = bins.apply(lambda x: f'{x.left}~{x.right}')
+
+
+df = pd.read_csv('datas.csv')
+
+# "컬럼명": "타입"의 딕셔너리 구성
+for column, dtype in df.dtypes.items():
+    if dtype == 'object':
+        pseudonymize_object(df, column)
+    elif dtype == 'int64' or dtype == 'float64':
+        pseudonymize_numeric(df, column)
 
 df.to_csv('masked_datas.csv', index=False)
