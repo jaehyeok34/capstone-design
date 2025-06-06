492e5750922b58df7841191bbb645c708e35d844
jaehyeok34 <hyeok9942@gmail.com>
2025-06-06 23:50:06 +0900

    test

--- matching.py
+++ matching.py
@@ -4,5 +4,5 @@ from api_gateway_utils import publish_event
 publish_event(
     name='matching.match.request',
     path_variable='matching_result',
-    json_data=json.dumps(['mk_data1_20250605075435758569_20250605075844024088', 'mk_data3_20250605083838189599_20250605083851269105'])
+    json_data=json.dumps(['mk_data1_20250606143652778103_20250606143654201587', 'mk_data3_20250606143605551165_20250606143607741380'])
 )
--- pii.py
+++ pii.py
@@ -1,7 +1,18 @@
 from api_gateway_utils import publish_event
-dataset_info = 'data3_20250606093621444759'
+import requests
+
+url = 'http://localhost:1780'
+
+def csv_register():
+    # /csv/register
+    file = {'file': open('data3.csv', 'rb')}
+    res = requests.post(url+'/csv/register', files=file)
+    print(res.text)
+    return res.text
+
+
 
 publish_event(
     name='pii.detection.request',
-    path_variable=dataset_info
+    path_variable=csv_register()
 )
