5e04d3e8ea9ce6831e97a957f09245c257a5a0e3
jaehyeok34 <hyeok9942@gmail.com>
2025-06-07 15:34:29 +0900

    pseudonymization server

--- config/env.py
+++ config/env.py
@@ -10,12 +10,3 @@ class Env(BaseModel):
     host: str = os.getenv('HOST')
     port: str = os.getenv('PORT')
     service_name: str = os.getenv('SERVICE_NAME')
-
-    db_host: str = os.getenv('DB_HOST')
-    db_port: int = int(os.getenv('DB_PORT'))
-    db_user: str = os.getenv('DB_USER')
-    db_password: str = os.getenv('DB_PASSWORD')
-    db_name: str = os.getenv('DB_NAME')
-
-    sbert_model_path: str = os.getenv('SBERT_MODEL_PATH')
-    threshold: float = float(os.getenv('THRESHOLD'))
