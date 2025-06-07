4a9b02c078acbf9428880e4cf53d732cbbdb1a23
jaehyeok34 <hyeok9942@gmail.com>
2025-06-07 12:00:32 +0900

    pii detection server

--- dto/pii_detection_response.py
+++ dto/pii_detection_response.py
@@ -5,10 +5,10 @@ from pydantic.alias_generators import to_camel
 
 
 class PiiDetectionResponse(BaseModel):
-    dataset_info_list: List[str]
-    pii: List[str]
-
     model_config = ConfigDict(
         alias_generator=to_camel,
         populate_by_name=True,
     )
+    
+    dataset_info_list: List[str]
+    pii: List[str]
