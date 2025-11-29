f0c838b7dd42506d5e6e153cf0ac0eadc73db3ea
jaehyeok34 <hyeok9942@gmail.com>
2025-11-29 20:02:56 +0900

    기존 라우터 구조가 service의 메서드를 await으로 실행하고 있으나, 실제 service에서 동기적으로 브로커에 요청 및 대기를 하기 때문에 만약, 어떠한 문제로 produce, find, consume 등에서 timeout 만큼 대기하게 된다면 스레드가 대기하면서 모든 요청을 처리 하지 못하는 상태가 됨. 따라서, 실제 브로커에 요청 및 대기하는 코드를 running loop를 획득하여 병렬로 실행시킨 다음, 그 결과를 await으로 받게 변경하여 완전 비동기를 이룸

--- service/user_service.py
+++ service/user_service.py
@@ -1,8 +1,10 @@
+import asyncio
 import base64
 import json
 from typing import Dict, List
 from fastapi import UploadFile
 
+from py_client.message.message import Message
 from py_client.agent import Agent
 from utils import CLIENT_ID, HOST, PORT
 
@@ -10,259 +12,137 @@ class UserService:
     counter = 0
 
     async def convert_file(self, file: UploadFile) -> str | None:
-        topic_name = "convert"
-        partition = 1
+        header = {
+            "file.name": file.filename or "unknown",
+            "api.id": self.get_api_id()
+        }
 
-        with Agent.of(host=HOST, port=PORT, client_id=CLIENT_ID) as agent:
-            api_id = UserService.counter
-            UserService.counter += 1
-
-            header = {
-                "file.name": file.filename or "unknown",
-                "api.id": str(api_id)
-            }
-
-            response = agent.producer.syncProduce(topic_name=topic_name, partition=str(partition), header=header, payload=await file.read())
-            if error := response.get_header("error"):
-                print(f"? convert_file(): {error}")
-                return None
-
-            if not agent.find_and_seek(topic_name=topic_name, partition=str(-partition), condition=header):   
-                return None
-            
-            response = agent.consumer.consume(topic_name=topic_name, partition=str(-partition))[0]
-            if error := response.get_header("error"):
-                print(f"? convert_file(): {error}")
-                return None
-            
-            return response.payload.decode("utf-8")
+        if (response := await self.run_async(topic_name="convert", partition=1, header=header, payload=await file.read())) is None:
+            return None
+        
+        return response.payload.decode("utf-8")
  
     async def export_file(self, format: str, file_name: str):
-        topic_name = "convert"
-        partition = 2
+        header = {
+            "file.format": format,
+            "file.name": file_name,
+            "api.id": self.get_api_id()
+        }
 
-        with Agent.of(host=HOST, port=PORT, client_id=CLIENT_ID) as agent:
-            api_id = UserService.counter
-            UserService.counter += 1
-
-            header = {
-                "file.format": format,
-                "file.name": file_name,
-                "api.id": str(api_id)
-            }
-
-            response = agent.producer.syncProduce(topic_name=topic_name, partition=str(partition), header=header, payload=b"req")
-            if error := response.get_header("error"):
-                print(f"? export_file(): {error}")
-                return None
-
-            if not agent.find_and_seek(topic_name=topic_name, partition=str(-partition), condition=header):
-                return None
-            
-            response = agent.consumer.consume(topic_name=topic_name, partition=str(-partition))[0]
-            if error := response.get_header("error"):
-                print(f"? export_file(): {error}")
-                return None
-            
-            return response.payload
+        if (response := await self.run_async(topic_name="convert", partition=2, header=header)) is None:
+            return None
+        
+        return response.payload
 
     async def find_candidate_columns(self, files: List[UploadFile]) -> Dict[str, List[str]] | None:
-        topic_name = "join"
-        partition = 1
-
-        with Agent.of(host=HOST, port=PORT, client_id=CLIENT_ID) as agent:
-            api_id = str(UserService.counter)
-            UserService.counter += 1
-
-            header = {
-                "api.id": api_id
-            }
-
-            payload = json.dumps({
-                file.filename: base64.b64encode(await file.read()).decode("utf-8")
-                for file in files
-            })
-
-            response = agent.producer.syncProduce(topic_name=topic_name, partition=str(partition), header=header, payload=payload)
-            if error := response.get_header("error"):
-                print(f"? find_candidate_columns(): {error}")
-                return None
-            
-            if not agent.find_and_seek(topic_name=topic_name, partition=str(-partition), condition=header):
-                return None
-            
-            response = agent.consumer.consume(topic_name=topic_name, partition=str(-partition))[0]
-            if error := response.get_header("error"):
-                print(f"? find_candidate_columns(): {error}")
-                return None
-            
-            return json.loads(response.payload.decode("utf-8"))
+        header = {
+            "api.id": self.get_api_id()
+        }
+        payload = json.dumps({
+            file.filename: base64.b64encode(await file.read()).decode("utf-8")
+            for file in files
+        })
+        
+        if (response := await self.run_async(topic_name="join", partition=1, header=header, payload=payload)) is None:
+            return None
+        
+        return json.loads(response.payload.decode("utf-8"))
         
     async def create_project(self, project_name: str, candidate_columns: Dict[str, List[str]], files: List[UploadFile]) -> str | None:
-        topic_name = "join"
-        partition = 2
+        header = {
+            "api.id": self.get_api_id()
+        }
+        payload = json.dumps({
+            "projectName": project_name,
+            "candidateColumns": candidate_columns,
+            "files": {
+                file.filename: base64.b64encode(await file.read()).decode("utf-8")
+                for file in files
+            }
+        })
 
-        with Agent.of(host=HOST, port=PORT, client_id=CLIENT_ID) as agent:
-            api_id = str(UserService.counter)
-            UserService.counter += 1
-
-            header = {"api.id": api_id}
-            payload = json.dumps({
-                "projectName": project_name,
-                "candidateColumns": candidate_columns,
-                "files": {
-                    file.filename: base64.b64encode(await file.read()).decode("utf-8")
-                    for file in files
-                }
-            })
-
-            response = agent.producer.syncProduce(topic_name=topic_name, partition=str(partition), header=header, payload=payload)
-            if error := response.get_header("error"):
-                print(f"? create_project(): {error}")
-                return None
-            
-            if not agent.find_and_seek(topic_name=topic_name, partition=str(-partition), condition=header):
-                return None
-            
-            response = agent.consumer.consume(topic_name=topic_name, partition=str(-partition))[0]
-            if error := response.get_header("error"):
-                print(f"? create_project(): {error}")
-                return None
-            
-            return response.payload.decode("utf-8")
+        if (response := await self.run_async(topic_name="join", partition=2, header=header, payload=payload)) is None:
+            return None
+        
+        return response.payload.decode("utf-8")
 
     async def get_projects(self) -> str | None:
-        topic_name = "join"
-        partition = 3
+        header = {
+            "api.id": self.get_api_id()
+        }
 
-        with Agent.of(host=HOST, port=PORT, client_id=CLIENT_ID) as agent:
-            api_id = str(UserService.counter)
-            UserService.counter += 1
-
-            header = {"api.id": api_id}
-            response = agent.producer.syncProduce(topic_name=topic_name, partition=str(partition), header=header)
-            if error := response.get_header("error"):
-                print(f"? get_projects(): {error}")
-                return None
-            
-            if not agent.find_and_seek(topic_name=topic_name, partition=str(-partition), condition=header):
-                return None
-            
-            response = agent.consumer.consume(topic_name=topic_name, partition=str(-partition))[0]
-            if error := response.get_header("error"):
-                print(f"? get_projects(): {error}")
-                return None
-            
-            return response.payload.decode("utf-8")
+        if(response := await self.run_async(topic_name="join", partition=3, header=header)) is None:
+            return None
+        
+        return response.payload.decode("utf-8")
         
     async def create_ci(self, project_id: str) -> bool:
-        topic_name = "join"
-        partition = 4
+        header = {
+            "project.id": project_id,
+            "api.id": self.get_api_id()
+        }
 
-        with Agent.of(host=HOST, port=PORT, client_id=CLIENT_ID) as agent:
-            api_id = str(UserService.counter)
-            UserService.counter += 1
-
-            header = {
-                "project.id": project_id,
-                "api.id": api_id
-            }
-
-            response = agent.producer.syncProduce(topic_name=topic_name, partition=str(partition), header=header)
-            if error := response.get_header("error"):
-                print(f"? create_ci(): {error}")
-                return False
-            
-            if not agent.find_and_seek(topic_name=topic_name, partition=str(-partition), condition=header):
-                return False
-            
-            response = agent.consumer.consume(topic_name=topic_name, partition=str(-partition))[0]
-            if error := response.get_header("error"):
-                print(f"? create_ci(): {error}")
-                return False
-            
+        if (response := await self.run_async(topic_name="join", partition=4, header=header)) is None:
+            return False
+        
         return True
     
-    async def get_project(self, project_id: str) -> str | None:
-        topic_name = "join"
-        partition = 6
-
-        with Agent.of(host=HOST, port=PORT, client_id=CLIENT_ID) as agent:
-            api_id = str(UserService.counter)
-            UserService.counter += 1
-
-            header = {
-                "project.id": project_id,
-                "api.id": api_id
-            }
-
-            response = agent.producer.syncProduce(topic_name=topic_name, partition=str(partition), header=header)
-            if error := response.get_header("error"):
-                print(f"? get_project(): {error}")
-                return None
-            
-            if not agent.find_and_seek(topic_name=topic_name, partition=str(-partition), condition=header):
-                return None
-            
-            response = agent.consumer.consume(topic_name=topic_name, partition=str(-partition))[0]
-            if error := response.get_header("error"):
-                print(f"? get_project(): {error}")
-                return None
-            
-            return response.payload.decode("utf-8")
-        
     async def join(self, project_id: str) -> bool:
-        topic_name = "join"
-        partition = 5
+        header = {
+            "project.id": project_id,
+            "api.id": self.get_api_id()
+        }
 
-        with Agent.of(host=HOST, port=PORT, client_id=CLIENT_ID) as agent:
-            api_id = str(UserService.counter)
-            UserService.counter += 1
-
-            header = {
-                "project.id": project_id,
-                "api.id": api_id
-            }
-
-            response = agent.producer.syncProduce(topic_name=topic_name, partition=str(partition), header=header)
-            if error := response.get_header("error"):
-                print(f"? join(): {error}")
-                return False
-            
-            if not agent.find_and_seek(topic_name=topic_name, partition=str(-partition), condition=header):
-                return False
-            
-            response = agent.consumer.consume(topic_name=topic_name, partition=str(-partition))[0]
-            if error := response.get_header("error"):
-                print(f"? join(): {error}")
-                return False
-            
+        if (response := await self.run_async(topic_name="join", partition=5, header=header)) is None:
+            return False
+        
         return True
+
+    async def get_project(self, project_id: str) -> str | None:
+        header = {
+            "project.id": project_id,
+            "api.id": self.get_api_id()
+        }
+
+        if (response := await self.run_async(topic_name="join", partition=6, header=header)) is None:
+            return None
+        
+        return response.payload.decode("utf-8")
     
     async def get_result(self, project_id: str) -> bytes | None:
-        topic_name = "join"
-        partition = 7
+        header = {
+            "project.id": project_id,
+            "api.id": self.get_api_id()
+        }
 
+        if (response := await self.run_async(topic_name="join", partition=7, header=header)) is None:
+            return None
+        
+        return response.payload
+
+    def consume(self, topic_name: str, partition: int, header: Dict[str, str], payload = None, condition: Dict[str, str] | None = None) -> Message | None:
         with Agent.of(host=HOST, port=PORT, client_id=CLIENT_ID) as agent:
-            api_id = str(UserService.counter)
-            UserService.counter += 1
-
-            header = {
-                "project.id": project_id,
-                "api.id": api_id
-            }
-
-            response = agent.producer.syncProduce(topic_name=topic_name, partition=str(partition), header=header)
+            response = agent.producer.syncProduce(topic_name=topic_name, partition=str(partition), header=header, payload=payload)
             if error := response.get_header("error"):
-                print(f"? get_result(): {error}")
+                print(f"? consume(): {error}")
                 return None
             
-            if not agent.find_and_seek(topic_name=topic_name, partition=str(-partition), condition=header):
+            condition = header if condition is None else condition
+            if not agent.find_and_seek(topic_name=topic_name, partition=str(-partition), condition=condition):
                 return None
             
             response = agent.consumer.consume(topic_name=topic_name, partition=str(-partition))[0]
             if error := response.get_header("error"):
-                print(f"? get_result(): {error}")
+                print(f"? consume(): {error}")
                 return None
             
-            return response.payload
+            return response
+
+    def get_api_id(self) -> str:
+        api_id = str(UserService.counter)
+        UserService.counter += 1
+
+        return api_id
+    
+    def run_async(self, topic_name: str, partition: int, header: Dict[str, str], payload = None):
+        return asyncio.get_running_loop().run_in_executor(None, self.consume, topic_name, partition, header, payload)
